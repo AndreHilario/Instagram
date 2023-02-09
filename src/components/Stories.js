153ee84f9019ab57fc8dab2c33fcab1689c523b5
Andre Hilario <andre18hilario@gmail.com>
2023-02-09 08:54:03 -0300

    Version using map to add stories

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,16 +1,21 @@
 import Story from "./Story"
 
 export default function Stories(){
+
+    const stories = [
+        {url : "assets/img/9gag.svg", text : "9gag"},
+        {url : "assets/img/meowed.svg", text : "meowed"},
+        {url : "assets/img/barked.svg", text : "barked"},
+        {url : "assets/img/nathanwpylestrangeplanet.svg", text : "nathanwp..."},
+        {url : "assets/img/wawawicomics.svg", text : "wawawico..."},
+        {url : "assets/img/respondeai.svg", text : "respondeai"},
+        {url : "assets/img/filomoderna.svg", text : "filomoderna"},
+        {url : "assets/img/memeriagourmet.svg", text : "memeriag..."},
+    ]
     return (
         <div class="stories">
-            <Story url="assets/img/9gag.svg" text="9gag" />
-            <Story url="assets/img/meowed.svg" text="meowed" />
-            <Story url="assets/img/barked.svg" text="barked" />
-            <Story url="assets/img/nathanwpylestrangeplanet.svg" text="nathanwp..." />
-            <Story url="assets/img/wawawicomics.svg" text="wawawico..." />
-            <Story url="assets/img/respondeai.svg" text="respondeai" />
-            <Story url="assets/img/filomoderna.svg" text="filomoderna" />
-            <Story url="assets/img/memeriagourmet.svg" text="memeriag..." />
+            {stories.map((s) => <Story url={s.url} text={s.text}/>)}
+            
             <div class="setinha">
                 <ion-icon name="chevron-forward-circle" role="img" class="md hydrated" aria-label="chevron forward circle"></ion-icon>
             </div>
